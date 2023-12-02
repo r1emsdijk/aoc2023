8f468aa835945b8bdcfd5c1ec1966d7e7f6b6238
Chris van Riemsdijk <chris@klippa.com>
2023-12-02 12:13:17 +0100

    day 1 + 2

---
+++ src/bin/01.rs
@@ -0,0 +1,65 @@
+advent_of_code::solution!(1);
+
+fn parse_digits(t_num: &str) -> Vec<u32> {
+    t_num.chars().filter_map(|a| a.to_digit(10)).collect()
+}
+pub fn part_one(input: &str) -> Option<u32> {
+    let data: Vec<Vec<u32>> = input.split("\n").map(|s| parse_digits(s)).collect();
+    let result = data.into_iter().map(|a| match a.len() {
+        2.. => Vec::from([a[0], a[a.len() - 1]])
+            .iter()
+            .fold(0, |acc, elem| acc * 10 + *elem),
+        1 => Vec::from([a[0], a[0]])
+            .iter()
+            .fold(0, |acc, elem| acc * 10 + *elem),
+        0 => 0,
+    });
+
+    Some(result.sum())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let data = input
+        .split("\n")
+        .map(|line| {
+            line.to_string()
+                .replace("zero", "zero0zero")
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
+                .chars()
+                .filter_map(|c| c.to_digit(10))
+                .collect::<Vec<u32>>()
+        })
+        .map(|v| {
+            if v.len() < 1 {
+                return 0;
+            }
+            10 * v[0] + v[v.len() - 1]
+        })
+        .sum();
+    Some(data)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(319));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
---
+++ src/bin/02.rs
@@ -0,0 +1,79 @@
+use std::cmp::max;
+
+advent_of_code::solution!(2);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let data: Vec<Vec<&str>> = input
+        .lines()
+        .map(|s| s.split(&[':', ';', ','][..]).collect::<Vec<&str>>())
+        .collect();
+
+    let mut sum = 0;
+    for game in data {
+        let game_id: u32 = game[0]
+            .strip_prefix("Game ")?
+            .parse::<u32>()
+            .expect("Game ID");
+        let mut possible = true;
+        for g in &game[1..] {
+            let amount_color = &g.trim().split(" ").collect::<Vec<&str>>();
+            let amount = amount_color[0].parse::<i32>().expect("Not an integer");
+            possible = match &amount_color[1].chars().nth(0).unwrap() {
+                'b' => amount <= 14,
+                'g' => amount <= 13,
+                'r' => amount <= 12,
+                _ => panic!("Unknown color"),
+            };
+            if !possible {
+                break;
+            }
+        }
+        if possible {
+            sum += game_id;
+        }
+    }
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let data: Vec<Vec<&str>> = input
+        .lines()
+        .map(|s| s.split(&[':', ';', ','][..]).collect::<Vec<&str>>())
+        .collect();
+
+    let mut sum = 0;
+    for game in data {
+        let mut green = -1;
+        let mut red = -1;
+        let mut blue = -1;
+        for g in &game[1..] {
+            let amount_color = &g.trim().split(" ").collect::<Vec<&str>>();
+            let amount = amount_color[0].parse::<i32>().expect("Not an integer");
+            match &amount_color[1].chars().nth(0).unwrap() {
+                'b' => blue = max(amount, blue),
+                'g' => green = max(amount, green),
+                'r' => red = max(amount, red),
+                _ => panic!("Unknown color"),
+            };
+        }
+        sum += green * red * blue
+    }
+    Some(sum as u32)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(8));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
